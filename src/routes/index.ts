84f623da59ff3d718f7bee754037302cb3f87937
Yves <yvesgeraud98@gmail.com>
2026-03-23 11:26:01 -0600

    chore(debug): remover ruta de diagnostico

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,22 +4,5 @@ import dtDocumentoRoute from './dt_documento.route';
 
 export const router = express.Router();
 
-import { config } from '@/config/servidor.config';
-router.get('/debug-env', (req, res) => {
-  res.json({
-    config: {
-      dbConfig: config.db,
-      enviroment: config.nodeEnv,
-      isProd: config.esProduccion
-    },
-    processEnv: {
-      DB_HOST: process.env.DB_HOST,
-      DB_PORT: process.env.DB_PORT,
-      DB_USER: process.env.DB_USER,
-      NODE_ENV: process.env.NODE_ENV
-    }
-  });
-});
-
 router.use('/ct_tipo_documento', ctTipoDocumentoRoute);
 router.use('/dt_documento', dtDocumentoRoute);
